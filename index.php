20334e8c606e26d2b0f78206ae0990a89718bd3c
grant08604@gmail.com <grant08604@gmail.com>
2023-09-27 10:58:21 +0800

    added new image

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 <h1>Hello world! re1</h1>
 <p>This is the first file in my new Git Repo.</p>
 <img src="img_hello_world.jpg" alt="">
+<div><img src="img_hello_git.jpg" alt="Hello Git" style="width:100%;max-width:640px"></div>
 </body>
 </html>
