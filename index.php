52626666a1b32e84ad9154e035f80ad57ebb979e
grant08604@gmail.com <grant08604@gmail.com>
2023-09-27 11:06:04 +0800

    add word in line

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 </head>
 <body>
 
-<h1>Hello world! re1</h1>
+<h1>Hello world! re2</h1>
 <p>This is the first file in my new Git Repo.</p>
 <img src="img_hello_world.jpg" alt="">
 <div><img src="img_hello_git.jpg" alt="Hello Git" style="width:100%;max-width:640px"></div>
